3f9cdcbe21e75f58f1f7013e58cdfcd921f2cd64
Allison Falls <allison.falls2@gmail.com>
2021-09-03 17:43:29 -0400

    fixed

---
+++ src/main/java/Quote.java
@@ -0,0 +1,20 @@
+import java.io.Serializable;
+
+public class Quote implements Serializable {
+    private int id;
+    private String content;
+    private Author author;
+
+    public Quote(){
+    }
+
+    public Quote(int id, String content, Author author) {
+        this.id = id;
+        this.content = content;
+        this.author = author;
+    }
+
+
+
+}
+
