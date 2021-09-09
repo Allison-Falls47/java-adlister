64f330af3c4f5bd088194727dee14790609497c9
Allison Falls <allison.falls2@gmail.com>
2021-09-09 18:17:07 -0400

    had to create a new branch due to file errors

---
+++ src/main/java/jdbc/Config.java
@@ -0,0 +1,17 @@
+package jdbc;
+
+public class Config {
+
+    public String getUrl(){
+        return
+    }
+
+    public String getUser(){
+        return
+    }
+
+    public String getPassword(){
+        return
+    }
+
+}
